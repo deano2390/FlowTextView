80c4f708b3a203853f2fe1eb018674812e76db22
deano2390 <dw86@kent.ac.uk>
2014-06-24 16:10:40 +0100

    Adding sample images and hooking up some buttons

--- flowtextview-sample/src/main/java/com/dean/flowtextviewsampleapp/sample/MainActivity.java
+++ flowtextview-sample/src/main/java/com/dean/flowtextviewsampleapp/sample/MainActivity.java
@@ -2,18 +2,63 @@ package com.dean.flowtextviewsampleapp.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.deanwild.flowtext.FlowTextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private static final float defaultFontSize = 20.0f;
+
+    private FlowTextView flowTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FlowTextView flowTextView = (FlowTextView) findViewById(R.id. ftv);
+        flowTextView = (FlowTextView) findViewById(R.id. ftv);
         flowTextView.setText(getString(R.string.lorem));
 
+        Button btnIncreasefontSize = (Button) findViewById(R.id.btn_increase_font_size);
+        btnIncreasefontSize.setOnClickListener(this);
+        Button btnDecreasefontSize = (Button) findViewById(R.id.btn_decrease_font_size);
+        btnDecreasefontSize.setOnClickListener(this);
+        Button btnReset = (Button) findViewById(R.id.btn_reset);
+        btnReset.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.btn_increase_font_size:
+                increaseFontSize();
+                break;
+            case R.id.btn_decrease_font_size:
+                decreaseFontSize();
+                break;
+            case R.id.btn_reset:
+                reset();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void increaseFontSize(){
+        float currentFontSize = flowTextView.getTextsize();
+        flowTextView.setTextSize(currentFontSize+1);
+    }
+
+    private void decreaseFontSize(){
+        float currentFontSize = flowTextView.getTextsize();
+        flowTextView.setTextSize(currentFontSize-1);
+    }
+
+    private void reset(){
+        flowTextView.setTextSize(defaultFontSize);
     }
 }
