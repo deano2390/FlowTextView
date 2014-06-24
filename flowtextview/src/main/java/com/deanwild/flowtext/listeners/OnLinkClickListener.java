672130aa624305b80732b25d7e1f1d4756af6a06
deano2390 <dw86@kent.ac.uk>
2014-06-24 16:27:24 +0100

    begin refactor, moving inner classes into models folder, moving fields to top of class etc MAN this class was filthy

---
+++ flowtextview/src/main/java/com/deanwild/flowtext/listeners/OnLinkClickListener.java
@@ -0,0 +1,8 @@
+package com.deanwild.flowtext.listeners;
+
+/**
+* Created by Dean on 24/06/2014.
+*/
+public interface OnLinkClickListener {
+    public void onLinkClick(String url);
+}
---
+++ flowtextview/src/main/java/com/deanwild/flowtext/models/Box.java
@@ -0,0 +1,11 @@
+package com.deanwild.flowtext.models;
+
+/**
+* Created by Dean on 24/06/2014.
+*/
+public class Box {
+    public int topLeftx;
+    public int topLefty;
+    public int bottomRightx;
+    public int bottomRighty;
+}
