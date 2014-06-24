b1004135123c12088ba7e0fbbc68f6be847cb925
deano2390 <dw86@kent.ac.uk>
2014-06-24 15:46:52 +0100

    Adding lorem ipsum string to sample

--- flowtextview-sample/src/main/java/com/dean/flowtextviewsampleapp/sample/MainActivity.java
+++ flowtextview-sample/src/main/java/com/dean/flowtextviewsampleapp/sample/MainActivity.java
@@ -1,9 +1,9 @@
 package com.dean.flowtextviewsampleapp.sample;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+
+import com.deanwild.flowtext.FlowTextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,25 +12,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        FlowTextView flowTextView = (FlowTextView) findViewById(R.id. ftv);
+        flowTextView.setText(getString(R.string.lorem));
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
